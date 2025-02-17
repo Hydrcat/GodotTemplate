90dda50f245e9473585e969dd7f7e2576ba2303d
Hydrcat <a41xqb@outlook.com>
2025-02-17 23:25:56 +0800

    写一些文档名称

--- test/ui_test/game_panel/game_panel.gd
+++ test/ui_test/game_panel/game_panel.gd
@@ -7,9 +7,10 @@ func _ready() -> void:
 	UISignalBus.instance.add_unit_panel.connect(on_add_unit_panel)
 
 func on_add_unit_panel()->void:
-	var ps:=HydrcatFramework.asset_manager.load_asset("ui","unit_panel") as PackedScene
-	var node := ps.instantiate()
-	h_box_container.add_child(node)
+	# var ps:=HydrcatFramework.asset_manager.load_asset("ui","unit_panel") as PackedScene
+	# var node := ps.instantiate()
+	# h_box_container.add_child(node)
+	open_panel("UnitPanel")
 
 func _on_back_pressed() -> void:
 	switch_to("StartPanel")
