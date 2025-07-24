9ea87630fb099489cd0f14ca9581f7207f81b373
Hydrcat <a41xqb@outlook.com>
2025-07-24 22:30:08 +0800

    优化

--- framework/hydrcat_framework.gd
+++ framework/hydrcat_framework.gd
@@ -2,7 +2,8 @@
 extends ManagerBase
 
 @export_category("设置")
-@export var need_gen:NeedGenHelper
+@export var need_gen: NeedGenHelper
+
 @export_category("管理器")
 @export var asset_manager: AssetManager
 @export var ui_manager: UIManager
@@ -10,10 +11,13 @@ extends ManagerBase
 @export var signal_bus_manager: SignalBusManager
 
 
+
+
 ## 注册指定的管理器
-func register_manager()->void:
+func register_manager() -> void:
+	
 	pass
 	
 ## 取消指定的管理器
-func unregister_manager()->void:
+func unregister_manager() -> void:
 	pass
